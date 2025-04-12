6e1ecf8093f8796f4770806dccd1d0e6e16e67be
Madam Vignesh <madamvignesh@gmail.com>
2025-04-12 07:50:19 +0530

    Fisrt Upload

---
+++ controller/customerController.js
@@ -0,0 +1,55 @@
+const initializeDatabase = require('../model/database.js');
+
+const getCustomer = async (req, res) => {
+    const db = await initializeDatabase(); // Initialize the database connection
+    const {current_id, user_type} = req.auth;
+    const {input_search} = req.body;
+    if (user_type !== 'Admin') {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+    if (input_search){
+        try{
+            const customer = await db.get(`SELECT * FROM customer WHERE name LIKE '%${input_search}%'`);            if (!customer) {
+                return res.status(404).json({ error: 'Customer not found' });
+            }
+            res.json(customer);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } else {
+        try {
+            const customer = await db.all('SELECT * FROM customer');
+            res.json(customer); 
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+}
+
+const getCustomerById = async (req, res) => {
+    const db = await initializeDatabase(); // Initialize the database connection
+    
+    const { id } = req.params;
+    const {current_id, user_type} = req.auth;
+    if (user_type !== 'Admin') {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+  
+    try {
+        const customer = await db.get('SELECT * FROM customer WHERE user_id = ?', [id]);
+        if (!customer) {
+            return res.status(404).json({ error: 'Customer not found' });
+        }
+        res.json(customer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports = {
+    getCustomer,
+    getCustomerById
+}  
---
+++ controller/hotelController.js
@@ -0,0 +1,48 @@
+const initializeDatabase = require('../model/database.js');
+
+const getAllHostels = async (req, res) => {
+    const db = await initializeDatabase(); // Initialize the database connection
+    let {input_search} = req.body;
+    if (input_search){
+        try{
+            const hostels = await db.all(`SELECT * FROM hostel WHERE name LIKE '%${input_search}%' OR address LIKE '%${input_search}%'`);
+            res.send(hostels);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } else {
+        try{
+            const hostels = await db.all('SELECT * FROM hostel');
+            res.send(hostels);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+}
+
+const getHostelById = async (req, res) => {
+    const db = await initializeDatabase(); // Initialize the database connection
+    const { id } = req.params;
+    
+    input_search = input_search.toLowerCase()
+
+    if (input_search){
+        try {
+            const hostel = await db.get('SELECT * FROM hostel WHERE hotel_id = ? OR name = ? OR address = ?', [id, input_search, input_search]);
+            if (!hostel) {
+                return res.status(404).json({ error: 'Hostel not found' });
+            }
+            res.json(hostel);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } 
+}
+
+module.exports = {
+    getAllHostels,
+    getHostelById
+}
