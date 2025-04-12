759274cb505dc7e30b339cae8d664ab9c8e107ed
Madam Vignesh <madamvignesh@gmail.com>
2025-04-12 08:44:46 +0530

    Debug the code

--- controller/hotelController.js
+++ controller/hotelController.js
@@ -2,7 +2,8 @@ const initializeDatabase = require('../model/database.js');
 
 const getAllHostels = async (req, res) => {
     const db = await initializeDatabase(); // Initialize the database connection
-    let {input_search} = req.body;
+    const {input_search} = req.body;
+
     if (input_search){
         try{
             const hostels = await db.all(`SELECT * FROM hostel WHERE name LIKE '%${input_search}%' OR address LIKE '%${input_search}%'`);
@@ -25,21 +26,17 @@ const getAllHostels = async (req, res) => {
 const getHostelById = async (req, res) => {
     const db = await initializeDatabase(); // Initialize the database connection
     const { id } = req.params;
-    
-    input_search = input_search.toLowerCase()
 
-    if (input_search){
-        try {
-            const hostel = await db.get('SELECT * FROM hostel WHERE hotel_id = ? OR name = ? OR address = ?', [id, input_search, input_search]);
-            if (!hostel) {
-                return res.status(404).json({ error: 'Hostel not found' });
-            }
-            res.json(hostel);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: error });
+    try {
+        const hostel = await db.get('SELECT * FROM hostel WHERE hotel_id = ? =', [id]);
+        if (!hostel) {
+            return res.status(404).json({ error: 'Hostel not found' });
         }
-    } 
+        res.json(hostel);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error });
+    }
 }
 
 module.exports = {
