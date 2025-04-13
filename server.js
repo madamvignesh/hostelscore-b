b9bc4b3fbdef15d9b66689d66de5089d30b9a3b9
Madam Vignesh <madamvignesh@gmail.com>
2025-04-13 09:37:20 +0530

    Debug database

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
         });
 
         // Start the server
-        const PORT = 3000;
+        const PORT = 3001;
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
